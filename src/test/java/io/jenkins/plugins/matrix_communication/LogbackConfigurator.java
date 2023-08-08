e0d09a2f5fddea6af801ad0be0f3e80ecbc448f7
Réda Housni Alaoui <reda-alaoui@hey.com>
2023-08-08 10:57:08 +0200

    Fix compilation

--- src/test/java/io/jenkins/plugins/matrix_communication/LogbackConfigurator.java
+++ src/test/java/io/jenkins/plugins/matrix_communication/LogbackConfigurator.java
@@ -3,6 +3,7 @@ package io.jenkins.plugins.matrix_communication;
 import ch.qos.logback.classic.BasicConfigurator;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.Context;
 import java.util.Optional;
 
 /**
@@ -11,13 +12,14 @@ import java.util.Optional;
 public class LogbackConfigurator extends BasicConfigurator {
 
   @Override
-  public ExecutionStatus configure(LoggerContext lc) {
-    super.configure(lc);
+  public ExecutionStatus configure(Context context) {
+    super.configure(context);
     Level rootLevel =
         Optional.ofNullable(System.getenv("MATRIX_COMMUNICATION_PLUGIN_TEST_LOG_ROOT_LEVEL"))
             .map(Level::valueOf)
             .orElse(Level.OFF);
-    lc.getLogger("ROOT").setLevel(rootLevel);
+    LoggerContext loggerContext = (LoggerContext) context;
+    loggerContext.getLogger("ROOT").setLevel(rootLevel);
     return ExecutionStatus.DO_NOT_INVOKE_NEXT_IF_ANY;
   }
 }
